ba0e1a9ad91db00959c6e0a1701b441ab20a485a
Patrick Thomson <patrickt@github.com>
2019-12-07 16:15:06 -0500

    Fix miscellaneous brokenness.
    * Changes the law notation to use Unicode EQUIVALENT TO.
    * Fixes the generalized definition of cata'.
    * Restores mysteriously-cut anamorphism content and institute a really
      dumb example.
    
    Hattip to @JeffreyBenjaminBrown. Fixes #5.

--- src/Part2.hs
+++ src/Part2.hs
@@ -32,38 +32,39 @@ data Expr a
 ten, add, call :: Term Expr
 ten  = In (Literal { intVal = 10 })
 add  = In (Ident { name = "add" })
-call = In (Call { func = add, args = [ten, ten]}) -- add(10, 10)
+call = In (Call { func = add, args = [ten, ten]})---add(10, 10)
+
+countNodes :: Expr Int -> Int
+
+countNodes (Unary _ arg)         = arg + 1
+countNodes (Binary left _ right) = left + right + 1
+countNodes (Call fn args)        = fn + sum args + 1
+countNodes (Index it idx)        = it + idx + 1
+countNodes (Paren arg)           = arg + 1
 
 type Algebra f a = f a -> a
 
 cata :: (Functor f) => Algebra f a -> Term f -> a
 cata f = out >>> fmap (cata f) >>> f
 
-countNodes :: Algebra Expr Int
-countNodes (Unary _ arg)         = arg + 1
-countNodes (Binary left _ right) = left + right + 1
-countNodes (Call fn args)        = fn + sum args + 1
-countNodes (Index it idx)        = it + idx + 1
-countNodes (Paren arg)           = arg + 1
-countNodes (Literal _)           = 1
-countNodes (Ident   _)           = 1
+prettyPrint :: Expr Doc -> Doc
 
+prettyPrint (Literal i) = P.int i
+prettyPrint (Ident s) = P.text s
 
-prettyPrint :: Algebra Expr Doc
-prettyPrint (Literal i)     = P.int i
-prettyPrint (Ident s)       = P.text s
--- f(a,b...)
-prettyPrint (Call f as)     = f <> P.parens (P.cat (P.punctuate ", " as))
--- a[b]
-prettyPrint (Index it idx)  = it <> P.brackets idx
--- op x
-prettyPrint (Unary op it)   = P.text op <> it
--- lhs op rhs
-prettyPrint (Binary l op r) = l <> P.text op <> r
--- (op)
-prettyPrint (Paren ex)      = P.parens ex
+prettyPrint (Call f as)     = f <> P.parens (mconcat (P.punctuate "," as))  ---f(a,b...)
+prettyPrint (Index it idx)  = it <> P.brackets idx                ---a[b]
+prettyPrint (Unary op it)   = (P.text op) <> it                   ---op x
+prettyPrint (Binary l op r) = l <> (P.text op) <> r               ---lhs op rhs
+prettyPrint (Paren exp)     = P.parens exp                        ---(op)
 
 type Coalgebra f a = a -> f a
 
 ana :: (Functor f) => Coalgebra f a -> a -> Term f
 ana f = In <<< fmap (ana f) <<< f
+
+nested :: Int -> Term Expr
+nested n = ana go n where
+  go :: Coalgebra Expr Int
+  go 0 = Literal n
+  go n = Paren (n - 1)
